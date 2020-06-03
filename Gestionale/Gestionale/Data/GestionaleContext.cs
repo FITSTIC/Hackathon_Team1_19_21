0ce8ff3059e6219f485368e4c9af8ec918b7f56d
Matteo-AA <matteo.calafato.studio@fitstic-edu.com>
2020-06-04 00:15:06 +0200

    Riaggiornato il progetto completo con le nuove pagine

---
+++ Gestionale/Gestionale/Data/GestionaleContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Gestionale.Data
+{
+    public class GestionaleContext : DbContext
+    {
+        public GestionaleContext (DbContextOptions<GestionaleContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Gestionale.Data.Corsi> Corsi { get; set; }
+    }
+}
