3175525635e1f0ade5674027bee798d5c0b778ec
Matteo-AA <matteo.calafato.studio@fitstic-edu.com>
2020-06-18 12:25:53 +0200

    implementazione searchbar

---
+++ Gestionale/Gestionale/wwwroot/js/customJS.js
@@ -0,0 +1,8 @@
+﻿function Search() {
+  $("#SearchBar").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $(".bodySearch .trSearch").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+  });
+};
